6c26393a4ecdc30787abda8d1ee026f9b2671cbe
Kanyezi <1214241948@qq.com>
2025-10-29 21:53:54 +0800

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	new file:   alacritty/alacritty.toml
    	new file:   kitty/kitty.config
    	modified:   nvim/lazy-lock.json
    	modified:   nvim/lua/core/options.lua
    	modified:   nvim/lua/plugins/cmp.lua
    	new file:   nvim/lua/plugins/everforest.lua

--- nvim/lua/core/options.lua
+++ nvim/lua/core/options.lua
@@ -7,5 +7,32 @@ opt.relativenumber = true
 opt.number = true
 
 -- 缩进
-opt.tabstop = 2
-opt.shiftwidth = 2
+opt.tabstop = 4
+opt.shiftwidth = 4
+
+-- 防止包裹
+opt.wrap = false
+
+-- 光标行高亮
+opt.cursorline = true
+
+-- 启用鼠标
+opt.mouse = "a"
+
+-- 系统剪贴板
+opt.clipboard:append("unnamedplus")
+
+-- 默认新窗口在右下
+opt.splitright = true
+opt.splitbelow = true
+
+-- 搜索忽略大小写
+opt.ignorecase = true
+opt.smartcase = true
+
+-- 外观
+opt.termguicolors = true
+opt.signcolumn = "yes"
+
+-- 隐藏模式显示（因为lualine已经有了）
+-- vim.opt.showmode = false
