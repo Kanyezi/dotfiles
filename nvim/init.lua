8d29e76db101969c467bac09eb4c7e99b8d9f9e2
Kanyezi <1214241948@qq.com>
2025-11-04 14:27:08 +0800

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	new file:   nvim/.gitignore
    	new file:   nvim/.neoconf.json
    	new file:   nvim/LICENSE
    	new file:   nvim/README.md
    	modified:   nvim/init.lua
    	modified:   nvim/lazy-lock.json
    	new file:   nvim/lazyvim.json
    	new file:   nvim/lua/config/autocmds.lua
    	new file:   nvim/lua/config/keymaps.lua
    	new file:   nvim/lua/config/lazy.lua
    	new file:   nvim/lua/config/options.lua
    	deleted:    nvim/lua/core/options.lua
    	deleted:    nvim/lua/plugins-setup.lua
    	deleted:    nvim/lua/plugins/cmp.lua
    	new file:   nvim/lua/plugins/compet.lua
    	new file:   nvim/lua/plugins/example.lua
    	deleted:    nvim/lua/plugins/leetcode.lua
    	deleted:    nvim/lua/plugins/lsp.lua
    	deleted:    nvim/lua/plugins/snacks.lua
    	deleted:    nvim/lua/plugins/treesitter.lua
    	new file:   nvim/stylua.toml
    	modified:   waybar/config.jsonc

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,11 +1,2 @@
--- ~/.config/nvim/init.lua
-
--- 设置一个Leader键，这里设为空格键
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
-
--- 导入核心配置
-require("core.options")
-
--- 导入并配置插件
-require("plugins-setup")
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
--- nvim/lua/plugins/snacks.lua
+++
@@ -1,5 +0,0 @@
--- ~/.config/nvim/lua/plugins/snacks.lua
-return {
-  'folke/snacks.nvim',
-  opts = { picker = {} },   -- 只启用 picker 模块
-}
